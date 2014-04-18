637421fce3c8bc6dd34f289def0e9fe2ca8b31dd
root <root@ubuntu10-04-4-x64.(none)>
2014-04-18 14:17:54 +0800

    add sql for mysql

---
+++ sql/SME_LIST.sql
@@ -0,0 +1,39 @@
+-- phpMyAdmin SQL Dump
+-- version 3.4.10.1deb1
+-- http://www.phpmyadmin.net
+--
+-- 主機: localhost
+-- 產生日期: 2014 年 04 月 18 日 14:01
+-- 伺服器版本: 5.5.31
+-- PHP 版本: 5.3.10-1ubuntu3.11
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- 資料庫: `cbm`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 表的結構 `SME_LIST`
+--
+
+CREATE TABLE IF NOT EXISTS `SME_LIST` (
+  `smeID` varchar(128) NOT NULL,
+  `host` varchar(15) NOT NULL,
+  `status` varchar(8) NOT NULL,
+  `timstamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
